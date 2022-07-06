84a6897dfcc71fd49d43c119fc29b3e95e7f7124
JOhnRobertThe <johnrobert776@gmail.com>
2022-07-06 18:33:14 +0200

    sista uppgiften

--- API.py
+++ API.py
@@ -1,9 +1,20 @@
 import requests
 
-def get_phone(var):
-    phone = requests.get(f'http://localhost:5000/api?action=phone&name={variable1}')
-    return phone.text
+"""def get_phone(name):
+    print(name)
+    phone = requests.get(f'http://localhost:5000/api?action=phone&name={name}')
+    print("hej " + phone)
+
+    return  #phone.text
+    """
     
-def get_name(var):
-    name = requests.get(f'http://localhost:5000/api?action=name&phone=0435-4355438')
+    
+def get_name(phone):
+    print("hej " + phone)
+    name = requests.get(f'http://localhost:5000/api?action=name&phone={phone})
     return name.text
+    
+def get_phone(name):
+    print("hej " + phone)
+    phone = requests.get(f'http://localhost:5000/api?action=phone&name={name})
+    return phone.text
